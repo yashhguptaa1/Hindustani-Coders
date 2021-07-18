ad81e149688adade28147630ff157443883ac4d7
yashhguptaa1 <yashhguptaa1@gmail.com>
2021-07-18 14:00:07 +0530

    Implemented hamburger menu, transition in mob-view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import styled from 'styled-components';
+import { useState } from 'react';
 import Sidebar from './Components/Sidebar';
 
+import styled from 'styled-components';
+import { IconButton } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+
 const App = ()=> {
+
+  const [navToggle, setNavToggle] = useState(false);
+
   return (
     <div className="App">
-      <Sidebar/>
+      <Sidebar navToggle={navToggle} />
+
+      <div className="ham-burger-menu">
+          <IconButton onClick={() => setNavToggle(!navToggle)}>
+              <MenuIcon />
+          </IconButton>
+        </div>
     </div>
   );
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components';
 import Navigation from './Navigation';
 
-const Sidebar=(props)=> {
+const Sidebar=({navToggle})=> {
     return (
-        <SidebarStyled>
+        <SidebarStyled className={`${navToggle ? 'nav-toggle' : ''}`}>
             <Navigation />
         </SidebarStyled>
     )
@@ -16,6 +16,11 @@ const SidebarStyled = styled.div`
     height: 100vh;
     background-color: var(--sidebar-dark-color);
     overflow: hidden;
+    transition: all .4s ease-in-out;
+    @media screen and (max-width:1200px){
+        transform: translateX(-100%);
+        z-index: 20;
+    }
 `;
 
 export default Sidebar;
